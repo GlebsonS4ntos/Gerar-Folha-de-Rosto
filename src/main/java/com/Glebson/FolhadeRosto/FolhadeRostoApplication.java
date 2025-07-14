b3f14c88127a00a2b0dafef5974d8b047c6245e0
Glebson Santos <glebsonsantos@outlook.pt>
2025-07-14 14:41:12 -0300

    init: create project

---
+++ src/main/java/com/Glebson/FolhadeRosto/FolhadeRostoApplication.java
@@ -0,0 +1,13 @@
+package com.Glebson.FolhadeRosto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolhadeRostoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolhadeRostoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Glebson/FolhadeRosto/FolhadeRostoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Glebson.FolhadeRosto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolhadeRostoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

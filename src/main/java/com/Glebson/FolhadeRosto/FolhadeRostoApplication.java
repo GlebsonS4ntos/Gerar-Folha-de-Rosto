4860d83a8089c012380b8b9021fec692f609b762
Glebson Santos <glebsonsantos@outlook.pt>
2025-07-19 09:54:20 -0300

    feat: add async suport

--- src/main/java/com/Glebson/FolhadeRosto/FolhadeRostoApplication.java
+++ src/main/java/com/Glebson/FolhadeRosto/FolhadeRostoApplication.java
@@ -2,8 +2,10 @@ package com.Glebson.FolhadeRosto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class FolhadeRostoApplication {
 
 	public static void main(String[] args) {
